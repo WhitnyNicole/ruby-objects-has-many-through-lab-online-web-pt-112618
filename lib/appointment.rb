8e60245b1fbe23784f8096c9d54ff767ff636654
Whitny Edwards <wne4e2@gmail.com>
2019-01-12 23:22:05 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++
@@ -1,9 +0,0 @@
-class Appointment
-  attr_accessor :doctor, :patient, :name 
-
-  def initialize(name, patient)
-    @name = name 
-    self.patient = patient  
-    patient.add_appointment(self) 
-  end 
-end
