89128b5fa4121490b9d7fdd6dd9b813427d34bf6
Whitny Edwards <wne4e2@gmail.com>
2019-01-12 23:16:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,22 +1,10 @@
 class Song
-
   attr_accessor :name, :artist, :genre
 
-  ALL = []
-
-  def initialize(name="", artist="", genre="")
-    @name = name
-    @artist = artist
-    @genre = genre
-    ALL << self
-  end
-
-  def self.all
-    ALL
-  end
-
-  def artist
-    @artist
+  def initialize(name, genre)
+      @name = name
+      @genre = genre
+      genre.add_song(self)
   end
 
 end
