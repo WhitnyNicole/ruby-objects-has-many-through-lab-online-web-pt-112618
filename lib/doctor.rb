8d4893c803cb1f32fa1f375c400fcdb5c7cd9718
Whitny Edwards <wne4e2@gmail.com>
2019-01-12 23:14:10 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,32 +1,32 @@
-# class Doctor
+class Doctor
 
-#   attr_reader :name, :appointment, :patient
+  attr_reader :name, :appointment, :patient
 
-#   @@all_doc = []
+  @@all_doc = []
 
-#   def initialize(name)
-#     @name = name
-#     @@all_doc << self
-#   end
+  def initialize(name)
+    @name = name
+    @@all_doc << self
+  end
 
-#   def self.all
-#     @@all_doc
-#   end
+  def self.all
+    @@all_doc
+  end
 
-#   def new_appointment(patient, date)
-#     Appointment.new(patient, self, date)
-#   end
+  def new_appointment(patient, date)
+    Appointment.new(patient, self, date)
+  end
 
-#   def appointments
-#     Appointment.all.select do |appointment|
-#       appointment.doctor == self
-#     end
-#   end
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
+    end
+  end
 
-#   def patients
-#     appointments.map do |appointment|
-#       appointment.patients
-#     end
-#   end
+  def patients
+    appointments.map do |appointment|
+      appointment.patients
+    end
+  end
 
-# end
+end
