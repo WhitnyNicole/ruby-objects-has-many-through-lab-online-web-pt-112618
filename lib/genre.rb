bda42d0e2e33139550fa5f4896d9de34616a4fb3
Whitny Edwards <wne4e2@gmail.com>
2019-01-12 23:31:46 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,11 +1,17 @@
 class Genre
 
+  @@all = []
   attr_reader :name, :genre
   attr_accessor :song
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+  
+  def self.all
+    @@all
+  end 
 
   def songs
     Song.all.select do |song|
